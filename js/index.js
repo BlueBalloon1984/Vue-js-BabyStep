3ce5d1fd97053e5c276ad0ae1ae13be4d9ce97db
BlueBalloon1984 <jyyt528@gmail.com>
2016-11-20 15:33:55 +0800

    watch-Im 15:33

--- js/index.js
+++ js/index.js
@@ -6,28 +6,29 @@ var watchExampleVM = new Vue({
   },
   watch:{
     question:function(newQusetion){
-    this.answer = 'Waitong for you to stop typing...'
-    this.getAnswer()
-    }
-  },
-  method:{
-    getAnswer:  _.debounce(
-      function(){
-        var vm = this;
-        if(this.question.indexOf('?') === -1){
-          vm.answer = 'Question usually contain a question mark.'
-          return
-        }
-        vm.answer = 'Thinking...'
-        axios.get('https://yesno.wtf/api')
-          .then(function (response){
-            vm.answer = _.capitalize(response.data.answer)
-          })
-          .catch(function (error){
-            vm.answer = 'Error!Could not reach the api.'+error
-          })
-      },
-      500
-    )
+        this.answer = 'Waitong for you to stop typing...'
+        this.getAnswer()
+      }
+    },
+  methods: {
+    getAnswer: _.debounce(
+          function () {
+            var vm = this
+            if (this.question.indexOf('?') === -1) {
+              vm.answer = 'Questions usually contain a question mark. ;-)'
+              return
+            }
+            vm.answer = 'Thinking...'
+            axios.get('https://yesno.wtf/api')
+              .then(function (response) {
+                vm.answer = _.capitalize(response.data.answer)
+              })
+              .catch(function (error) {
+                vm.answer = 'Error! Could not reach the API. ' + error
+              })
+          },
+          // 这是我们为用户停止输入等待的毫秒数
+          500
+        )
   }
 })
